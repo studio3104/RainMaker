afb2a70cd8240a61c06562643d6a186b77c4788a
studio3104 <studio3104.com@gmail.com>
2021-11-04 07:18:48 +0900

    Implement data collector

---
+++ collector.py
@@ -0,0 +1,20 @@
+import dataclasses
+
+from libraries.exchanges.bitflyer import BitFlyerRealTime, Ticker, PublicChannel, ProductCode
+from libraries.exchanges.bitflyer.models import TickerTable
+
+client = BitFlyerRealTime()
+
+
+def _handler(ticker: Ticker) -> None:
+    t = TickerTable(**dataclasses.asdict(ticker))
+    t.save()
+
+
+def run() -> None:
+    client.subscribe(PublicChannel.lightning_ticker, ProductCode.BTC_JPY, _handler)
+    client.start()
+
+
+if __name__ == '__main__':
+    run()
