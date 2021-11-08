3bbe238ff8a50270213cd41763338d223aec5e34
studio3104 <studio3104.com@gmail.com>
2021-11-09 05:42:32 +0900

    Rename methods

--- libraries/signals/support_resistance/fractal.py
+++ libraries/signals/support_resistance/fractal.py
@@ -13,28 +13,28 @@ class Fractal(SupportResistance):
         for i in range(2, self.df.shape[0] - 2):
             level = None
 
-            if self.is_support(i):
+            if self._is_support(i):
                 level = int(self.df['Low'][i])
                 self._supports.append(level)
 
-            if self.is_resistance(i):
+            if self._is_resistance(i):
                 level = int(self.df['High'][i])
                 self._resistances.append(level)
 
-            if level is not None:  # and self.is_far_from_level(mean, level):
+            if level is not None and self._is_far_from_level(mean, level):
                 self._levels.append(level)
 
-    def is_support(self, i: int) -> bool:
+    def _is_support(self, i: int) -> bool:
         return (
                 self.df['Low'][i] < self.df['Low'][i - 1] < self.df['Low'][i - 2] and
                 self.df['Low'][i] < self.df['Low'][i + 1] < self.df['Low'][i + 2]
         )
 
-    def is_resistance(self, i: int) -> bool:
+    def _is_resistance(self, i: int) -> bool:
         return (
                 self.df['High'][i] > self.df['High'][i - 1] > self.df['High'][i - 2] and
                 self.df['High'][i] > self.df['High'][i + 1] > self.df['High'][i + 2]
         )
 
-    def is_far_from_level(self, mean: numpy.float64, level: int, ) -> bool:
+    def _is_far_from_level(self, mean: numpy.float64, level: int, ) -> bool:
         return numpy.sum([abs(level - x) < mean for x in self._levels]) == 0
